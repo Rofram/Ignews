c256fd20c7a0bec795754639c612bbc763d29382
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-28 00:52:02 -0300

    feat: 🎉 implemented formatprice

---
+++ src/utils/formatPrice.ts
@@ -0,0 +1,8 @@
+export default function formatPrice(price: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(price / 100);
+}
