002a2b4fd68bb3379f2f9629d01cdd9e8ab02ebe
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-28 01:05:41 -0300

    feat: 🎉 implemented signIn callback

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,4 +14,45 @@ export default NextAuth({
     // ...add more providers here
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        if(email) {
+          await fauna.query(
+            q.If(
+              q.Not(
+                q.Exists(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(email)
+                  )
+                )
+              ),
+              q.Create(
+                q.Collection('users'),
+                { data: { email } }
+              ),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            )
+          )
+        } else {
+          throw new Error('Email is required')
+        }
+
+        return true
+      }
+      catch(err) {
+        console.log(err)
+
+        return false
+      }
+    }
+  }
 })
