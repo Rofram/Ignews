318beaddfe97897f68821de56623189cc1948a77
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-28 00:53:29 -0300

    feat: 🎉 implemented faunadb service

--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,5 +1,8 @@
 import { Client } from 'faunadb'
 
 export const fauna = new Client({
-  secret: process.env.FAUNADB_KEY!
+  secret: process.env.FAUNADB_KEY!,
+  domain: "db.us.fauna.com",
+  port: 443,
+  scheme: 'https',
 })
