83b68f44514643c5a92e526ef46092c27bb890e4
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-28 00:57:41 -0300

    feat: 🎉 implemented handleSubscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { useSession, signIn } from 'next-auth/react'
+import { getStripeInstance } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 export type SubscribeButtonProps = {
@@ -5,10 +7,34 @@ export type SubscribeButtonProps = {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data, status } = useSession()
+
+  async function handleSubscribe() {
+    if(status != 'authenticated') {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await fetch('/api/subscribe', {
+        method: 'POST',
+      })
+
+      const { sessionId } = await response.json()
+
+      const stripe = await getStripeInstance()
+
+      await stripe?.redirectToCheckout({ sessionId })
+    } catch(err) {
+      alert(err)
+    }
+  }
+
   return (
     <button 
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
